6d6f57bcb7f2d2dc3628b8829527424fb14d70f1
Saurab Dulal <sdulal@memphis.edu>
2023-07-28 15:28:09 +0000

    working pipe

--- new-suppression-rl/base.py
+++ new-suppression-rl/base.py
@@ -9,6 +9,8 @@ from agent import Agent
 import environment as e
 from environment import _Embedding, MulticastEnvironment
 import tensorflow as tf
+from pathlib import Path as p
+
 
 fifo_suppression_value = 'fifo_suppression_value' #we don't care if this file is chaged because we write to this file
 fifo_object_details = 'fifo_object_details'
@@ -62,63 +64,79 @@ class Memory:
 
 class MulticastSuppression:
   def __init__(self, fifo_object_details, fifo_suppression_value, alpha=1e-5, n_actions=4, default_timer=10):
-      self.env = MulticastEnvironment('multi-cast')
-      self.agent = Agent(n_actions=n_actions)
-      self.embed = _Embedding(number_of_document=200)
-      self.prev_suppression_timer = default_timer #this time is in ms
-      self.fifo_suppression_value = fifo_suppression_value
-      self.fifo_object_details = fifo_object_details
-      try:
-          os.mkfifo(self.fifo_suppression_value)
-          os.mkfifo(self.fifo_object_details)
-      except FileExistsError:
-          pass
+    self.env = MulticastEnvironment('multi-cast')
+    self.agent = Agent(n_actions=n_actions)
+    self.embed = _Embedding(number_of_document=200)
+    self.prev_suppression_timer = default_timer #this time is in ms
+    self.fifo_suppression_value = fifo_suppression_value
+    self.fifo_object_details = fifo_object_details
+    try:
+        os.mkfifo(self.fifo_suppression_value)
+        os.mkfifo(self.fifo_object_details)
+    except FileExistsError:
+        pass
     
 
 # INCOMPLETE
 if __name__ == "__main__":
 
-  mcast_sup = MulticastSuppression(fifo_object_details, fifo_suppression_value)
-  memory = Memory()
+  print ("The reinforcement is starting........")
+  print("*********", p.cwd(), "*************") # for debugging
+
+
+  # mcast_sup = MulticastSuppression(fifo_object_details, fifo_suppression_value)
+  # memory = Memory()
   
-  done = False
-  duplicates = 0
-  object_name = None
-  score = 0
-  counter = 0
+  # done = False
+  # duplicates = 0
+  # object_name = None
+  # score = 0
+  # counter = 0
 
   # memory.prev_suppression_time = int(mcast_sup.env.sample_action()*100)
-
-  new_observation = mcast_sup.embed.get_embedding("/home/abc", 1.5, 2.3, 3)
-  
-  new_observation = tf.math.reduce_mean(new_observation, axis=0)
-
-  reward = mcast_sup.env.get_reward(1.5, 2.3, 3)
-
-  action = mcast_sup.agent.choose_action(new_observation)
-
+  # new_observation = mcast_sup.embed.get_embedding("/home/abc", 1.5, 2.3, 3)
+  # new_observation = tf.math.reduce_mean(new_observation, axis=0)
+  # reward = mcast_sup.env.get_reward(1.5, 2.3, 3)
+  # action = mcast_sup.agent.choose_action(new_observation)
   # final_action = mcast_sup.env.take_action(action)
+  # print(reward, action, action)
+  # exit()
 
-  print(reward, action, action)
+  # print ("Suppression time value from the action: ", action)
+  # score += reward
 
-  exit()
+  # print (score, action)
+ 
+  # create fifo for communication. Make sure that RL module is started before C++
+  # C++ will write to an open fifo 
+  try:
+    os.mkfifo(fifo_object_details, mode=0o777)
+  except OSError as oe:
+    if oe.errno != errno.EEXIST:
+      print("some shit happened")
+      raise
 
-  print ("Suppression time value from the action: ", action)
-  score += reward
+  counter = 0 # this is only for testing, modify accordingly
+  while True:
+    try:
+      read_pipe = os.open(fifo_object_details, os.O_RDONLY)
+      bytes = os.read(read_pipe, 50)
+      if len(bytes) == 0:
+        break
+      print("Received from NDN/C++", bytes)
+      os.close(read_pipe)
+      
+      write_pipe = os.open(fifo_object_details, os.O_WRONLY)
+    
+      response = "{}".format(counter)
+      counter=counter+1
+      os.write(write_pipe, response.encode())
 
-  print (score, action)
+      os.close(write_pipe)
 
-    # while(1):
-    #     print ("The reinforcement is starting........")
-    #     try:
-    #         with open ("fifo_object_details", 'rb') as f:
-    #             content = f.read().decode('utf-8', 'ignore')
-    #             # get name_prefix, srtt, rtt (if available), ewma
-
-                   
-    #     except Exception as e:
-    #         print (e)
-
+    except Exception as e:
+      print ("exception: ", e)
+      
     #     new_observation = mcast_sup.embed.get_embedding(name_prefix, ewma, srtt, rtt)
 
     #     reward = mcast_sup.env._get_reward(name_prefix, ewma, srtt, rtt)
@@ -140,4 +158,3 @@ if __name__ == "__main__":
 
     #     print ("Counter:", counter, "Score: ", score,  "Reward: ", reward)
     #     fifo_write(action)
-
